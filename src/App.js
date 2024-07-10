5a656ddf4a1b6f7bd3a656aeef340b233bd7e4bc
R-Arun2 <itsarunkumar299@gmail.com>
2024-07-10 15:59:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+
+    
+    <div className="App">
+
+   <h1><strong>Task Manager</strong></h1>
+      
+      <TodoWrapper  />
+
+      
+    </div>
+  );
+}
+
+export default App;
